2775044bb0d75be3bc7cf70261b64d824dcab8de
문정훈 <mjh090801abc@gmail.com>
2026-05-12 20:10:51 +0900

    test commit

---
+++ src/main/java/com/example/PartTrip/PartTripApplication.java
@@ -0,0 +1,13 @@
+package com.example.PartTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartTripApplication.class, args);
+	}
+
+}
