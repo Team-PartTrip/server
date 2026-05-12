e2eee00b9eaf9d33255c20a3568afd4f59528ae4
문정훈 <mjh090801abc@gmail.com>
2026-05-12 20:36:12 +0900

    @Table을 이용해 테이블 이름을 넣고 id값 자동으로 1씩 증가하도록 코드 작성

---
+++ src/main/java/com/example/PartTrip/entity/UserManage.java
@@ -0,0 +1,18 @@
+package com.example.PartTrip.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_manage")
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserManage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
