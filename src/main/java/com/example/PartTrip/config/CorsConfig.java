0cf8df7f2b13128bfcfa877425107555ab64af86
문정훈 <mjh090801abc@gmail.com>
2026-05-14 20:10:38 +0900

    CORS로 프론트 포트 번홀랑 연결을 위한 코드 작성

---
+++ src/main/java/com/example/PartTrip/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.example.PartTrip.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://localhost:5173"
+        ));
+
+        config.setAllowedMethods(List.of())
+
+
+        ));
+
+    }
+}
