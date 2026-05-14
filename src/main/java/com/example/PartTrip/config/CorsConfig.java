95f80fca9ab84899db9a29d0bf6cf66a8b0c0bd7
문정훈 <mjh090801abc@gmail.com>
2026-05-14 20:51:40 +0900

    주석으로 코드 설명 및 정리

--- src/main/java/com/example/PartTrip/config/CorsConfig.java
+++ src/main/java/com/example/PartTrip/config/CorsConfig.java
@@ -10,14 +10,18 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    // CORS 설정 제공 객체를 만들어 반환하는 메서드
     public CorsConfigurationSource corsConfigurationSource() {
+        // CORS 설정 객체
         CorsConfiguration config = new CorsConfiguration();
 
+        // 허용할 프론트 포트
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:5173"
         ));
 
+        // 허용할 HTTP 메서드
         config.setAllowedMethods(List.of(
                 "GET",
                 "POST",
@@ -27,10 +31,14 @@ public class CorsConfig {
                 "OPTIONS"
         ));
 
+        // 모든 헤더 허용
         config.setAllowedHeaders(List.of("*"));
+        // 쿠키/인증 허용
         config.setAllowCredentials(true);
 
+        // 모든 URL마다 CORS 규칙 적용을 저장하는 객체
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // (경로, 적용할 규칙) : 모든 URL에 config 규칙 허용
         source.registerCorsConfiguration("/**", config);
 
         return source;
