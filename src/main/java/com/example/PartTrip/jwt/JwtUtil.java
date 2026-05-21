e35bb3ba533731a72d90013110030e2b3039436a
문정훈 <mjh090801abc@gmail.com>
2026-05-21 23:31:11 +0900

    Access Token 메서드를 만드는 코드 작성

--- src/main/java/com/example/PartTrip/jwt/JwtUtil.java
+++ src/main/java/com/example/PartTrip/jwt/JwtUtil.java
@@ -1,6 +1,10 @@
 package com.example.PartTrip.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JwtUtil {
@@ -8,4 +12,14 @@ public class JwtUtil {
     private final String SECRET_KEY = "parttrip-secret-key-parttrip-secret-key";
     private final long ACCESS_TOKEN_TIME = 1000 * 60 * 60;
     private final long REFRESH_TOKEN_TIME = 1000L * 60 * 60 * 24 * 7;
+
+    public String createAccessToken(Long userId, String userMail) {
+        return Jwts.builder()
+                .setSubject(userMail)
+                .claim("userId", userId)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
 }
