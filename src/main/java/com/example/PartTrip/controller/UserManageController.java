7b3fed337b507a9f1cd61bbd2fcad690384913a3
2junhyeon <2junhyeon@gmail.com>
2026-05-21 18:07:43 +0900

    UserService를 private로 받은 뒤 @PostMapping 사용으로 JSON으로 요청을 받고 Service의 함수를 사용하여 요청을 반환

---
+++ src/main/java/com/example/PartTrip/controller/UserManageController.java
@@ -0,0 +1,25 @@
+package com.example.PartTrip.controller;
+
+import com.example.PartTrip.dto.UserManageDto;
+import com.example.PartTrip.entity.UserManage;
+import com.example.PartTrip.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserManageController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public UserManage signup(@RequestBody UserManageDto dto) {
+
+        return userService.saveUser(dto);
+    }
+
+}
