8acaac45111efa8068390e4570f7fb02b6115bb5
문정훈 <mjh090801abc@gmail.com>
2026-05-26 13:54:44 +0900

    Entity 변경에 따른 DTO의 필드 변경

--- src/main/java/com/example/PartTrip/dto/UserManageDto.java
+++ src/main/java/com/example/PartTrip/dto/UserManageDto.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserManageDto {
-    private Long userId;
+    private String userId;
     private String userPwd;
     private String userMail;
     private String nickName;
