5ec79e75864611279a8512e4b093d156ff97067d
문정훈 <mjh090801abc@gmail.com>
2026-05-26 14:03:16 +0900

    id를 받기 위한 코드 추가 + 비밀번호 암호화 코드 추가 + 주석 추가

--- src/main/java/com/example/PartTrip/service/SignUpService.java
+++ src/main/java/com/example/PartTrip/service/SignUpService.java
@@ -1,35 +1,52 @@
 package com.example.PartTrip.service;
 
-
 import com.example.PartTrip.dto.SignUpRequestDto;
 import com.example.PartTrip.entity.UserEntity;
 import com.example.PartTrip.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SignUpService {
 
-
     private final UserRepository userRepository;
 
-    // 회원 가입
+    // 비밀번호 암호화 객체
+    private final PasswordEncoder passwordEncoder;
 
+    // 회원가입
     public UserEntity saveUser(SignUpRequestDto dto) {
+
+        // UserEntity 객체 생성
         UserEntity user = new UserEntity();
 
-        user.setUserPwd(dto.getUserPwd());
+        // 로그인 아이디 저장
+        user.setUserId(dto.getUserId());
+
+        // 비밀번호 암호화 후 저장
+        user.setUserPwd(passwordEncoder.encode(dto.getUserPwd()));
+
+        // 이메일 저장
         user.setUserMail(dto.getUserMail());
+
+        // 닉네임 저장
         user.setNickName(dto.getNickName());
+
+        // 회원가입 방식 저장
         user.setSignUpDivision(dto.getSignUpDivision());
+
+        // 전화번호 저장
         user.setPhnNumber(dto.getPhnNumber());
+
+        // 국가 저장
         user.setMyCountry(dto.getMyCountry());
+
+        // 여행 타입 저장
         user.setTravelType(dto.getTravelType());
 
-
+        // DB 저장 후 반환
         return userRepository.save(user);
     }
-
-
 }
