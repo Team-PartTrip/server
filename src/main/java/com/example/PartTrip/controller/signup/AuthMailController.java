b1e61beb2e39c81df3265b4b4d67484b8510808b
문정훈 <mjh090801abc@gmail.com>
2026-05-28 17:13:46 +0900

    인증 코드 보내기 위한 코드 작성

--- src/main/java/com/example/PartTrip/controller/signup/AuthMailController.java
+++ src/main/java/com/example/PartTrip/controller/signup/AuthMailController.java
@@ -1,6 +1,10 @@
 package com.example.PartTrip.controller.signup;
 
+import com.example.PartTrip.dto.signup.EmailSendRequestDto;
+import com.example.PartTrip.service.signup.MailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/auth/email")
 public class AuthMailController {
+
+    private final MailService mailService;
+
+    @PostMapping("/send")
+    public String sendCode(@RequestBody EmailSendRequestDto dto) {
+
+        mailService.sendCode(dto.getEmail());
+
+        return "인증번호가 전송되었습니다.";
+
+    }
+
 }
