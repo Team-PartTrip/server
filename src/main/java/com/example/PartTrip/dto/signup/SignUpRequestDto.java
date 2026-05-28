ef52c359ca812ca8e135cc25711cf7b730998f9c
문정훈 <mjh090801abc@gmail.com>
2026-05-28 17:51:56 +0900

    필요없는 컬럼들 삭제

--- src/main/java/com/example/PartTrip/dto/signup/SignUpRequestDto.java
+++ src/main/java/com/example/PartTrip/dto/signup/SignUpRequestDto.java
@@ -1,33 +1,15 @@
 package com.example.PartTrip.dto.signup;
 
+import lombok.Getter;
+import lombok.Setter;
 
-import lombok.AllArgsConstructor;
-import lombok.Data; // Getter + Setter = Data
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-
-@Data // getter + setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
+@Setter
 public class SignUpRequestDto {
+
     private String userId;
     private String userPwd;
     private String userMail;
-    private String nickName;
     private String signUpDivision;
-    private String phnNumber;
     private String myCountry;
-    private String travelType;
-    private LocalDateTime createDate;
-
-
-
-
-
-
-
-
-
 }
-
