97ed846eb7081a5ebb66421b7b65786e6eafb782
문정훈 <mjh090801abc@gmail.com>
2026-05-28 12:16:51 +0900

    나머지 인증번호, 인증 완료 여부, 만료 시간 등을 컬럼을 지정 + 주석 추가

--- src/main/java/com/example/PartTrip/entity/signup/EmailVerificationEntity.java
+++ src/main/java/com/example/PartTrip/entity/signup/EmailVerificationEntity.java
@@ -18,8 +18,20 @@ import java.time.LocalDateTime;
 public class EmailVerificationEntity {
 
     @Id
+    // 사용자 이메일(PK)
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    // 인증번호
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    // 인증 완료 여부
+    @Column(name = "verified", nullable = false)
+    private boolean verified;
+
+    // 인증번호 만료 시간
+    @Column(name = "expired_at", nullable = false)
+    private LocalDateTime expiredAt;
 
 }
