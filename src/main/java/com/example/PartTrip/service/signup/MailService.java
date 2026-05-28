de4e3c3f1fda1e1fcb07ce45ee285f50ade430c3
문정훈 <mjh090801abc@gmail.com>
2026-05-28 12:36:56 +0900

    인증 이메일, 인증 코드, 만료 시간등을 설정한 뒤 entity에 저장하여 DB로 저장되도록 코드 작성

--- src/main/java/com/example/PartTrip/service/signup/MailService.java
+++ src/main/java/com/example/PartTrip/service/signup/MailService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Random;
 
 @Service
@@ -21,6 +22,15 @@ public class MailService {
         String code = String.valueOf(new Random().nextInt(900000) + 100000);
 
         EmailVerificationEntity entity = new EmailVerificationEntity();
+        // 사용자가 이력한 이메일을 Entity에 넣음
+        entity.setEmail(email);
+        // 인증번호 6자리도 Entity에 넣음
+        entity.setCode(code);
+        // 사용자가 인증번호를 입력해서 검증된 상태가 아니므로 false로 저장
+        entity.setVerified(true);
+        // 인증 만료 시간(현재 시간에서 5분 더한 시간)
+        entity.setExpiredAt(LocalDateTime.now().plusMinutes(5));
+        emailVerificationRepository.save(entity);
     }
 
 }
