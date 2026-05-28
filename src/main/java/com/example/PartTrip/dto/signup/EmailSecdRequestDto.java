e5613527bd4a1754a941e93048a2dc0d2e1b2691
문정훈 <mjh090801abc@gmail.com>
2026-05-28 12:20:43 +0900

    인증 보낼 때 받는 이메일을 위한 코드 작성

---
+++ src/main/java/com/example/PartTrip/dto/signup/EmailSecdRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.PartTrip.dto.signup;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailSecdRequestDto {
+
+    // 인증 보낼 때 받는 이메일
+    private String email;
+
+}
