63dd2e48f3d75a18d59b81c51751661cb0cfebb7
문정훈 <mjh090801abc@gmail.com>
2026-06-05 00:45:38 +0900

    회원가입을 하기 위해 id, pwd를 친 후 이메일 인증을 하려고할 때 회원가입 정보를 저장할 임시 저장소 생성을 위한 entity 기본설정

---
+++ src/main/java/com/example/PartTrip/entity/signup/PendingSignUpEntity.java
@@ -0,0 +1,16 @@
+package com.example.PartTrip.entity.signup;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pending_signup")
+@Getter
+@Setter
+@NoArgsConstructor
+public class PendingSignUpEntity {
+
+}
