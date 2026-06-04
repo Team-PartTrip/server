f82913abfe5568308b97b3c33b1ad37f1a474844
문정훈 <mjh090801abc@gmail.com>
2026-06-05 00:53:17 +0900

    실제 회원가입에 있는 코드의 컬럼을 가져와 임시 저장소 완성

--- src/main/java/com/example/PartTrip/entity/signup/PendingSignUpEntity.java
+++ src/main/java/com/example/PartTrip/entity/signup/PendingSignUpEntity.java
@@ -1,10 +1,14 @@
 package com.example.PartTrip.entity.signup;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "pending_signup")
@@ -12,5 +16,26 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class PendingSignUpEntity {
+
+    @Id
+    @Column(name = "user_mail", nullable = false)
+    private String userMail;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "user_pwd", nullable = false)
+    private String userPwd;
+
+    @Column(name = "signup_division")
+    private String signupDivision;
+
+    @Column(name = "my_country")
+    private String myCountry;
+
+    // 임시 회원가입 만료 시간(10분)
+    @Column(name = "expired_at", nullable = false)
+    private LocalDateTime expiredAt;
+
 
 }
