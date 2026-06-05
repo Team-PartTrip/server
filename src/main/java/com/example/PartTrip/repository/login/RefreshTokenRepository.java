7d3c1d2d742d328dec4ba4c5f204dac521584b46
문정훈 <mjh090801abc@gmail.com>
2026-06-05 17:51:03 +0900

    역할을 구분하기 위해 클래스명 변경 + entity 변경에 따른 Repository 상속 받는 클래스명 변경

--- src/main/java/com/example/PartTrip/repository/login/RefreshTokenRepository.java
+++ src/main/java/com/example/PartTrip/repository/login/RefreshTokenRepository.java
@@ -1,15 +1,15 @@
 package com.example.PartTrip.repository.login;
 
-import com.example.PartTrip.entity.login.RefreshToken;
+import com.example.PartTrip.entity.login.RefreshTokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Long> {
 
     // user_id로 사용자가 이미 로그인한 토큰이 있는지 확인할 때 사용
-    Optional<RefreshToken> findByUserId(Long userId);
+    Optional<RefreshTokenEntity> findByUserId(Long userId);
 
     // 토큰 재발급 요청이 왔을 때 DB에 존재하는 토큰 확인할 때 사용
-    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+    Optional<RefreshTokenEntity> findByRefreshToken(String refreshToken);
 }
