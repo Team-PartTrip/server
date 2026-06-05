a42c52a4a15d635ae5b5c623a060a26d21868618
문정훈 <mjh090801abc@gmail.com>
2026-06-05 19:48:53 +0900

    엔티티 타입의 변경으로 인한 타입 변환

--- src/main/java/com/example/PartTrip/repository/login/RefreshTokenRepository.java
+++ src/main/java/com/example/PartTrip/repository/login/RefreshTokenRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Long> {
 
     // user_id로 사용자가 이미 로그인한 토큰이 있는지 확인할 때 사용
-    Optional<RefreshTokenEntity> findByUserId(Long userId);
+    Optional<RefreshTokenEntity> findByUserId(String userId);
 
     // 토큰 재발급 요청이 왔을 때 DB에 존재하는 토큰 확인할 때 사용
     Optional<RefreshTokenEntity> findByRefreshToken(String refreshToken);
