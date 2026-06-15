9eb16ecd7b7d82c2027a8107ef3ac83ef355e38a
문정훈 <mjh090801abc@gmail.com>
2026-06-15 19:48:30 +0900

    jwt 필터를 생성하는 코드 작성

---
+++ src/main/java/com/example/PartTrip/jwt/JwtAuthFilter.java
@@ -0,0 +1,29 @@
+package com.example.PartTrip.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component // 스프링이 자동으로 관리하는 객체 등록
+@RequiredArgsConstructor
+// OncePerRequestFilter : 요청 1개당 필터 1번 실행
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+
+    }
+}
