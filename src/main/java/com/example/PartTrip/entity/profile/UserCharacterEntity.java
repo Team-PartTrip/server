d7b4cc74a8049dae71db8383f797a1d1d578743b
2junhyeon <2junhyeon@gmail.com>
2026-06-17 20:29:15 +0900

    유저 캐릭터 엔티티 외래키 연결

--- src/main/java/com/example/PartTrip/entity/profile/UserCharacterEntity.java
+++ src/main/java/com/example/PartTrip/entity/profile/UserCharacterEntity.java
@@ -10,11 +10,23 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class UserCharacterEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_character_id")
     private Long userCharacterId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserProfileEntity user;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "character_id", nullable = false)
+    private CharacterInfoEntity character;
 
+    @Column(name = "user_level", nullable = false)
+    private Integer userLevel;
+
+    @Column(name = "character_point", nullable = false)
+    private Integer characterPoint;
 }
