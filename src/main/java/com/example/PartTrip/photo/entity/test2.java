f5530a01b18142b33a98992eaac9bf5a066b0c52
cringstrawberry <kimminji056050@gmail.com>
2026-06-18 23:16:33 +0900

    dd

--- src/main/java/com/example/PartTrip/photo/entity/test2.java
+++
@@ -1,9 +0,0 @@
-package com.example.PartTrip.photo.entity;
-
-import org.springframework.http.converter.json.GsonBuilderUtils;
-
-public class test2 {
-    public static void main(String[] args) {
-        System.out.println("test2");
-    }
-}
