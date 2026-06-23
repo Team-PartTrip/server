064f43e1fa3a517ff307d3fac7330287b591a569
김찬우 <chanwoo219@gmail.com>
2026-06-23 19:32:52 +0900

    비밀번호 변경을 위한 Dto

---
+++ src/main/java/com/example/PartTrip/dto/password/PasswordResetRequestDto.java
@@ -0,0 +1,19 @@
+package com.example.PartTrip.dto.password;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PasswordResetRequestDto {
+
+    // 비밀번호를 변경할 이메일
+    private String email;
+
+    // 새 비밀번호
+    private String newPassword;
+
+    // 새 비밀번호 재입력
+    private String confirmPassword;
+
+}
