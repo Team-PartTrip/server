107c12f5b3be22436936c8b3fa4fc8fb98c2d28f
문정훈 <mjh090801abc@gmail.com>
2026-06-23 23:44:50 +0900

    이메일 관련 에러 수정

--- src/main/java/com/example/PartTrip/service/signup/MailService.java
+++ src/main/java/com/example/PartTrip/service/signup/MailService.java
@@ -24,30 +24,47 @@ public class MailService {
         String code = String.valueOf(new Random().nextInt(900000) + 100000);
 
         EmailVerificationEntity entity = new EmailVerificationEntity();
-        // 사용자가 이력한 이메일을 Entity에 넣음
+
+        // 사용자가 입력한 이메일의 앞뒤 공백 제거
+        email = email.trim();
+
+        // 사용자가 입력한 이메일을 Entity에 넣음
         entity.setEmail(email);
+
         // 인증번호 6자리도 Entity에 넣음
         entity.setCode(code);
+
         // 사용자가 인증번호를 입력해서 검증된 상태가 아니므로 false로 저장
         entity.setVerified(false);
+
         // 인증 만료 시간(현재 시간에서 5분 더한 시간)
         entity.setExpiredAt(LocalDateTime.now().plusMinutes(5));
+
         emailVerificationRepository.save(entity);
 
         SimpleMailMessage message = new SimpleMailMessage();
 
         message.setTo(email);
-        message.setSubject("[PartTrip] 이메일 인증번호]");
-        message.setText("인증번호는 " + code + " + 입니다. 5분 안에 입력해주세요");
+
+        // 메일 제목
+        message.setSubject("[PartTrip] 이메일 인증번호");
+
+        // 메일 내용
+        message.setText("인증번호는 " + code + " 입니다. 5분 안에 입력해주세요.");
 
         mailSender.send(message);
     }
 
-
     // 이메일로 받은 인증번호를 입력했을 때 실행됨
     public void verifyCode(EmailVerifyRequestDto dto) {
 
-        EmailVerificationEntity entity = emailVerificationRepository.findById(dto.getEmail())
+        // 사용자가 입력한 이메일 앞뒤 공백 제거
+        String email = dto.getEmail().trim();
+
+        // 사용자가 입력한 인증번호 앞뒤 공백 제거
+        String code = dto.getCode().trim();
+
+        EmailVerificationEntity entity = emailVerificationRepository.findById(email)
                 .orElseThrow(() -> new IllegalArgumentException("인증번호를 먼저 요청해주세요."));
 
         // DB에 저장된 만료 시간을 가져오고 현재 시간을 가져와서 비교
@@ -56,20 +73,28 @@ public class MailService {
         }
 
         // 사용자가 입력한 인증 코드와 DB에 저장된 인증 코드가 다르다면 예외 출력
-        if (!entity.getCode().equals(dto.getCode())) {
+        if (!entity.getCode().equals(code)) {
             throw new IllegalArgumentException("인증번호가 일치하지 않습니다.");
         }
 
+        // 인증 완료 처리
         entity.setVerified(true);
+
         emailVerificationRepository.save(entity);
 
     }
 
     public boolean isVerified(String email) {
+
+        // 사용자가 입력한 이메일 앞뒤 공백 제거
+        email = email.trim();
+
         // DB에서 해당 이메일 인증 정보를 찾음
         return emailVerificationRepository.findById(email)
+
                 // 찾으면 그 Entity의 verified값을 꺼냄
                 .map(EmailVerificationEntity::isVerified)
+
                 .orElse(false);
     }
 
