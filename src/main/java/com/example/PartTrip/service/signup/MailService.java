b7e1c1453a9791e581f8bbf309b0401ebdceb6a4
문정훈 <mjh090801abc@gmail.com>
2026-06-24 14:05:58 +0900

    fix: mail service conflict

--- src/main/java/com/example/PartTrip/service/signup/MailService.java
+++ src/main/java/com/example/PartTrip/service/signup/MailService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+
 import java.time.LocalDateTime;
 import java.util.Random;
 
