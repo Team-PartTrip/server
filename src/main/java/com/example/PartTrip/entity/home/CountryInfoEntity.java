925d92d2f2210515d8de87ab090be11fd100ab5b
문정훈 <mjh090801abc@gmail.com>
2026-06-25 18:01:02 +0900

    메인 상단 국가 정보를 나타내는 entity 코드 작성

--- src/main/java/com/example/PartTrip/entity/home/CountryInfoEntity.java
+++ src/main/java/com/example/PartTrip/entity/home/CountryInfoEntity.java
@@ -18,4 +18,28 @@ public class CountryInfoEntity {
     @Column(name = "country_info_id")
     private Long countryInfoId;
 
+    // 나라 이름
+    @Column(name = "country_name", nullable = false)
+    private String countryName;
+
+    // 도시 이름
+    @Column(name = "city_name", nullable = false)
+    private String cityName;
+
+    // 메인 사진 주소
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    // 환율 표시
+    @Column(name = "exchange_rate_text")
+    private String exchangeRateText;
+
+    // 현지 시간
+    @Column(name = "local_time_text")
+    private String localTimeText;
+
+    // 문화 요약
+    @Column(name = "summary", length = 1000)
+    private String summary;
+
 }
