825a5e31ad6fefb7665c5d79d0aee4f66dbfa996
문정훈 <mjh090801abc@gmail.com>
2026-06-25 23:42:38 +0900

    요청에 응답할 수 있도록 DTO 추가

---
+++ src/main/java/com/example/PartTrip/dto/main/FestivalResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.PartTrip.dto.main;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FestivalResponseDto {
+
+    private String title;
+    private String category;
+    private String description;
+    private String startDate;
+    private String startTime;
+    private String location;
+    private String imageUrl;
+}
