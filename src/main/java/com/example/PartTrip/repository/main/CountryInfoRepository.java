6d587dd7da01fd30229679b6eecb688908d0238b
문정훈 <mjh090801abc@gmail.com>
2026-06-25 23:39:51 +0900

    각각의 Name을 활용하여 조회할 수 있도록 코드 추가

--- src/main/java/com/example/PartTrip/repository/main/CountryInfoRepository.java
+++ src/main/java/com/example/PartTrip/repository/main/CountryInfoRepository.java
@@ -3,6 +3,9 @@ package com.example.PartTrip.repository.main;
 import com.example.PartTrip.entity.main.CountryInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CountryInfoRepository extends JpaRepository<CountryInfoEntity, Long> {
 
+    Optional<CountryInfoEntity> findByCountryName(String countryName);
 }
--- src/main/java/com/example/PartTrip/repository/main/PopulationInfoRepository.java
+++ src/main/java/com/example/PartTrip/repository/main/PopulationInfoRepository.java
@@ -3,5 +3,9 @@ package com.example.PartTrip.repository.main;
 import com.example.PartTrip.entity.main.PopulationInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PopulationInfoRepository extends JpaRepository<PopulationInfoEntity, Long> {
+
+    List<PopulationInfoEntity> findByCountryName(String countryName);
 }
