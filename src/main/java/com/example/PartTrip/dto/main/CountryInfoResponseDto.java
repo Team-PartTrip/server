780bc10340853a7d1ccfb0f6c1755ea91c97d171
문정훈 <mjh090801abc@gmail.com>
2026-06-25 23:42:48 +0900

    요청에 응답할 수 있도록 DTO 추가

---
+++ src/main/java/com/example/PartTrip/dto/main/CountryInfoResponseDto.java
@@ -0,0 +1,14 @@
+package com.example.PartTrip.dto.main;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CountryInfoResponseDto {
+
+    private String countryName;
+    private String cityName;
+    private String imageUrl;
+    private String summary;
+}
