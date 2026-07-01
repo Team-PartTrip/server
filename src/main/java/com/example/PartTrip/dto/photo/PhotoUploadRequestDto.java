8d65f306f6c5e0ee2b55cf09bf927f32d18f6805
cringstrawberry <kimminji056050@gmail.com>
2026-07-01 12:05:25 +0900

    feat: 가이드 카메라 API 요청/응답 DTO 추가

--- src/main/java/com/example/PartTrip/dto/photo/PhotoUploadRequestDto.java
+++ src/main/java/com/example/PartTrip/dto/photo/PhotoUploadRequestDto.java
@@ -1,5 +1,7 @@
 package com.example.PartTrip.dto.photo;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,13 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 public class PhotoUploadRequestDto {
     private String travelId;
+
+    @NotNull(message = "이미지 파일은 필수입니다.")
     private MultipartFile imageFile;
+
+    @NotBlank(message = "위도는 필수입니다.")
     private String latitude;
+
+    @NotBlank(message = "경도는 필수입니다.")
     private String longitude;
 }
