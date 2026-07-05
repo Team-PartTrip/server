8369b544057547125a10e85420197f3906e9b77d
문정훈 <mjh090801abc@gmail.com>
2026-07-05 18:38:01 +0900

    국가 정보 조회를 위한 코드 작성

--- src/main/java/com/example/PartTrip/controller/main/MainController.java
+++ src/main/java/com/example/PartTrip/controller/main/MainController.java
@@ -1,15 +1,27 @@
 package com.example.PartTrip.controller.main;
 
+import com.example.PartTrip.dto.main.CountryInfoResponseDto;
+import com.example.PartTrip.service.main.MainService;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/main")
 public class MainController {
 
-    @GetMapping("/api/main")
-    public String mainPage() {
-        return "메인 페이지 접근 성공";
+    private final MainService mainService;
+
+    // 국가 정보 조회
+    @GetMapping("/country-info")
+    public CountryInfoResponseDto getCountryInfo(
+            @RequestParam String countryName
+    ) {
+        return mainService.getCountryInfo(countryName);
     }
 
 }
