18c5452dd34d1f6972da8537e66ffb6cd74696ee
문정훈 <mjh090801abc@gmail.com>
2026-07-05 18:35:26 +0900

    국가정보를 가져오기 위한 로직 작성

--- src/main/java/com/example/PartTrip/service/main/MainService.java
+++ src/main/java/com/example/PartTrip/service/main/MainService.java
@@ -1,5 +1,7 @@
 package com.example.PartTrip.service.main;
 
+import com.example.PartTrip.dto.main.CountryInfoResponseDto;
+import com.example.PartTrip.entity.main.CountryInfoEntity;
 import com.example.PartTrip.repository.main.CountryInfoRepository;
 import com.example.PartTrip.repository.main.FestivalRepository;
 import com.example.PartTrip.repository.main.PopulationInfoRepository;
@@ -13,5 +15,19 @@ public class MainService {
     private final CountryInfoRepository countryInfoRepository;
     private final PopulationInfoRepository populationInfoRepository;
     private final FestivalRepository festivalRepository;
+
+    // 국가 정보 조회
+    public CountryInfoResponseDto getCountryInfo(String countryName) {
+
+        CountryInfoEntity country = countryInfoRepository.findByCountryName(countryName)
+                .orElseThrow(() -> new IllegalArgumentException("국가 정보를 찾을 수 없습니다."));
+
+        return new CountryInfoResponseDto(
+                country.getCountryName(),
+                country.getCityName(),
+                country.getImageUrl(),
+                country.getSummary()
+        );
+    }
 
 }
