578d4b068578e82183620e76f9b45b6b2515fc6c
문정훈 <mjh090801abc@gmail.com>
2026-07-06 20:46:00 +0900

    최근 여행 검색 기록 저장을 위한 Dto 생성

---
+++ src/main/java/com/example/PartTrip/dto/main/search/RecentSearchRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.PartTrip.dto.main.search;
+
+import lombok.Getter;
+
+@Getter
+public class RecentSearchRequestDto {
+
+    private Long userId;
+    private Long countryInfoId;
+
+}
