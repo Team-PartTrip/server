a2fd4466a7dd2b92595a22d9208ee1d940f823e3
문정훈 <mjh090801abc@gmail.com>
2026-07-06 23:20:58 +0900

    DB 기준으로 인기순을 조회하기 위한 코드 리펙토링

--- src/main/java/com/example/PartTrip/service/main/search/PopularPlaceService.java
+++ src/main/java/com/example/PartTrip/service/main/search/PopularPlaceService.java
@@ -1,6 +1,7 @@
 package com.example.PartTrip.service.main.search;
 
 import com.example.PartTrip.dto.main.search.PopularPlaceResponseDto;
+import com.example.PartTrip.repository.main.CountryInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,40 +11,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PopularPlaceService {
 
+    private final CountryInfoRepository countryInfoRepository;
+
     public List<PopularPlaceResponseDto> getPopularPlaces() {
 
-        return List.of(
-
-                new PopularPlaceResponseDto(
-                        1L,
-                        "일본",
-                        "도쿄",
-                        "https://flagcdn.com/w320/jp.png"
-                ),
-
-                new PopularPlaceResponseDto(
-                        2L,
-                        "싱가포르",
-                        "Singapore",
-                        "https://flagcdn.com/w320/sg.png"
-                ),
-
-                new PopularPlaceResponseDto(
-                        3L,
-                        "태국",
-                        "Bangkok",
-                        "https://flagcdn.com/w320/th.png"
-                ),
-
-                new PopularPlaceResponseDto(
-                        4L,
-                        "프랑스",
-                        "Paris",
-                        "https://flagcdn.com/w320/fr.png"
-                )
-
-        );
-
+        return countryInfoRepository.findAll()
+                .stream()
+                .limit(4)
+                .map(country -> new PopularPlaceResponseDto(
+                        country.getCountryInfoId(),
+                        country.getCountryName(),
+                        country.getCityName(),
+                        country.getImageUrl()
+                ))
+                .toList();
     }
-
 }
