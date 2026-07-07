ed3fecacdedb13abb46c0bb0e2a19fe0be2bfe25
문정훈 <mjh090801abc@gmail.com>
2026-07-07 10:00:08 +0900

    그 전에 임시로 만들었던 코드 제거

--- src/main/java/com/example/PartTrip/mission/repository/MissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.PartTrip.mission.repository;
-
-import com.example.PartTrip.mission.entity.MissionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MissionRepository extends JpaRepository<MissionEntity, Long> {
-}
