77e2a60e16e9ae841f6a9891cb48679b9d413cd5
문정훈 <mjh090801abc@gmail.com>
2026-07-07 10:59:22 +0900

    더미 데이터 추가로 인한 로직 수정

--- src/main/java/com/example/PartTrip/service/main/TravelPlanService.java
+++ src/main/java/com/example/PartTrip/service/main/TravelPlanService.java
@@ -6,7 +6,6 @@ import com.example.PartTrip.entity.main.CountryInfoEntity;
 import com.example.PartTrip.entity.main.TravelPlanEntity;
 import com.example.PartTrip.repository.main.CountryInfoRepository;
 import com.example.PartTrip.repository.main.TravelPlanRepository;
-import com.example.PartTrip.service.mission.MissionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,20 +18,12 @@ public class TravelPlanService {
 
     private final TravelPlanRepository travelPlanRepository;
     private final CountryInfoRepository countryInfoRepository;
-    private final MissionService missionService;
 
     // 여행 일정 등록 또는 수정
     public DdayResponseDto saveTravelPlan(String userId, TravelPlanRequestDto dto) {
 
         TravelPlanEntity travelPlan = travelPlanRepository.findByUserId(userId)
                 .orElse(new TravelPlanEntity());
-
-        // 처음 등록인지 확인
-        boolean isNew = (travelPlan.getUserId() == null);
-
-        // 국가 변경 여부 확인
-        boolean countryChanged = !isNew &&
-                !travelPlan.getCountryName().equals(dto.getCountryName());
 
         travelPlan.setUserId(userId);
         travelPlan.setCountryName(dto.getCountryName());
@@ -41,14 +32,6 @@ public class TravelPlanService {
         travelPlan.setEndDate(dto.getEndDate());
 
         TravelPlanEntity savedTravelPlan = travelPlanRepository.save(travelPlan);
-
-        // 처음 등록 또는 국가가 변경된 경우에만 미션 생성
-        if (isNew || countryChanged) {
-            missionService.resetMission(
-                    userId,
-                    dto.getCountryName()
-            );
-        }
 
         return toDdayResponseDto(savedTravelPlan);
     }
@@ -90,6 +73,7 @@ public class TravelPlanService {
         );
     }
 
+    // 여행지 변경
     public void changeTravelCountry(Long travelPlanId, Long countryInfoId){
 
         TravelPlanEntity travelPlan =
@@ -100,7 +84,6 @@ public class TravelPlanService {
                 countryInfoRepository.findById(countryInfoId)
                         .orElseThrow(() -> new IllegalArgumentException("국가가 없습니다."));
 
-        // 같은 나라면 아무것도 안 함
         if (travelPlan.getCountryName().equals(country.getCountryName())) {
             return;
         }
@@ -109,11 +92,5 @@ public class TravelPlanService {
         travelPlan.setCityName(country.getCityName());
 
         travelPlanRepository.save(travelPlan);
-
-        // 국가가 바뀐 경우에만 미션 초기화
-        missionService.resetMission(
-                travelPlan.getUserId(),
-                country.getCountryName()
-        );
     }
 }
