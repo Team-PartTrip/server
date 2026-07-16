046417f875fe268b9b92ede208a2e29ed13dafb0
문정훈 <mjh090801abc@gmail.com>
2026-07-16 18:55:53 +0900

    사용자별 미션 생성을 위해 사용자의 미션이 존재하는지 확인하는 로직 추가

--- src/main/java/com/example/PartTrip/repository/mission/MissionRepository.java
+++ src/main/java/com/example/PartTrip/repository/mission/MissionRepository.java
@@ -13,4 +13,6 @@ public interface MissionRepository extends JpaRepository<MissionEntity, Long> {
 
     void deleteByUserId(String userId);
 
+    // 해당 사용자의 미션이 존재하는지 확인
+    boolean existsByUserId(String userId);
 }
