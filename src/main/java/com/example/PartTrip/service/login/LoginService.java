5c199184910bf487ef4b6fc539ddeccc1397b3a7
김찬우 <chanwoo219@gmail.com>
2026-07-24 20:19:07 +0900

    fix: LoginService에 @Transactional 추가 (리프레시 토큰 동시 요청 락 에러 대응)

--- src/main/java/com/example/PartTrip/service/login/LoginService.java
+++ src/main/java/com/example/PartTrip/service/login/LoginService.java
@@ -12,12 +12,14 @@ import com.example.PartTrip.repository.signup.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class LoginService {
 
     private final UserRepository userRepository;
