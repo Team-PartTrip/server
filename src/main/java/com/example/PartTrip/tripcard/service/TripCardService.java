44eb91975113a71902312a702eadb1cd8f4b7d27
2junhyeon <2junhyeon@gmail.com>
2026-08-26 23:23:11 +0900

    feat : 삭제용 추상메서드 추가

--- src/main/java/com/example/PartTrip/tripcard/service/TripCardService.java
+++ src/main/java/com/example/PartTrip/tripcard/service/TripCardService.java
@@ -1,15 +1,20 @@
 package com.example.PartTrip.tripcard.service;
 
+import com.example.PartTrip.tripcard.dto.response.TripCardDetailResponse;
 import com.example.PartTrip.tripcard.dto.response.TripCardResponse;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TripCardService {
 
     @Transactional(readOnly = true)
-    TripCardResponse getTripCard(Long tripCardId);
+    TripCardDetailResponse getTripCard(Long tripCardId);
 
     @Transactional(readOnly = true)
     List<TripCardResponse> getTripCards();
+
+    @Transactional
+    String deleteTripCard(Set<Long> tripCardIds);
 }
