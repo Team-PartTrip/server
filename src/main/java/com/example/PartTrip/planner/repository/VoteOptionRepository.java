dc03a02a64ab2bd09d879b23fff55a77e3963f3a
문정훈 <mjh090801abc@gmail.com>
2026-08-26 16:56:09 +0900

    feat: 여러 투표의 후보 일괄 조회 기능 추가

--- src/main/java/com/example/PartTrip/planner/repository/VoteOptionRepository.java
+++ src/main/java/com/example/PartTrip/planner/repository/VoteOptionRepository.java
@@ -10,6 +10,8 @@ public interface VoteOptionRepository extends JpaRepository<VoteOptionEntity, Lo
 
     List<VoteOptionEntity> findByVoteIdOrderByCreatedAtAsc(Long voteId);
 
+    List<VoteOptionEntity> findByVoteIdInOrderByCreatedAtAsc(List<Long> voteIds);
+
     // 삭제 시 어느 투표의 후보인지까지 확인한다
     Optional<VoteOptionEntity> findByOptionIdAndVoteId(Long optionId, Long voteId);
 
