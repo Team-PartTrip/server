2f4147ff49fcd28e28e020a4fec3cc6bbc49fa7c
2junhyeon <2junhyeon@gmail.com>
2026-08-26 23:24:05 +0900

    feat : 삭제 API 구현

--- src/main/java/com/example/PartTrip/tripcard/controller/TripCardController.java
+++ src/main/java/com/example/PartTrip/tripcard/controller/TripCardController.java
@@ -1,18 +1,17 @@
 package com.example.PartTrip.tripcard.controller;
 
+import com.example.PartTrip.tripcard.dto.response.TripCardDetailResponse;
 import com.example.PartTrip.tripcard.dto.response.TripCardResponse;
 import com.example.PartTrip.tripcard.service.TripCardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 
 @RestController
-@RequestMapping("api/trip-card")
+@RequestMapping("api/trip-cards")
 @RequiredArgsConstructor
 public class TripCardController {
 
@@ -26,8 +25,14 @@ public class TripCardController {
 
 
     @GetMapping("/{tripCardId}")
-    public TripCardResponse getTripCard(@PathVariable Long tripCardId) {
+    public TripCardDetailResponse getTripCard(@PathVariable Long tripCardId) {
         return tripCardService.getTripCard(tripCardId);
+    }
+
+
+    @DeleteMapping
+    public String deleteTripCard(@RequestBody Set<Long> tripCardIds) {
+        return tripCardService.deleteTripCard(tripCardIds);
     }
 
 
