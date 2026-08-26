b58683d4667feaded5b2f5c0ae56c9ee292e6b19
2junhyeon <2junhyeon@gmail.com>
2026-08-26 18:11:15 +0900

    feat : 여행카드 전체조회, 세부조회 API 구현

--- src/main/java/com/example/PartTrip/tripcard/controller/TripCardController.java
+++ src/main/java/com/example/PartTrip/tripcard/controller/TripCardController.java
@@ -1,9 +1,14 @@
 package com.example.PartTrip.tripcard.controller;
 
+import com.example.PartTrip.tripcard.dto.response.TripCardResponse;
 import com.example.PartTrip.tripcard.service.TripCardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -12,6 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class TripCardController {
 
     private final TripCardService tripCardService;
+
+
+    @GetMapping
+    public List<TripCardResponse> getTripCards() {
+        return tripCardService.getTripCards();
+    }
+
+
+    @GetMapping("/{tripCardId}")
+    public TripCardResponse getTripCard(@PathVariable Long tripCardId) {
+        return tripCardService.getTripCard(tripCardId);
+    }
 
 
 }
