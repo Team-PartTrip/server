7b2d99d47de2facac98e2d5f694eee645a48b1c3
2junhyeon <2junhyeon@gmail.com>
2026-08-29 17:21:54 +0900

    feat : 여행카드 사진 항목 API 및 경로 정렬

--- src/main/java/com/example/PartTrip/tripcard/controller/TripCardController.java
+++ src/main/java/com/example/PartTrip/tripcard/controller/TripCardController.java
@@ -1,21 +1,25 @@
 package com.example.PartTrip.tripcard.controller;
 
 import com.example.PartTrip.tripcard.dto.response.TripCardDetailResponse;
+import com.example.PartTrip.tripcard.dto.response.TripCardEntryResponse;
 import com.example.PartTrip.tripcard.dto.response.TripCardResponse;
+import com.example.PartTrip.tripcard.service.TripCardEntryService;
 import com.example.PartTrip.tripcard.service.TripCardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Set;
 
 
 @RestController
-@RequestMapping("api/trip-cards")
+@RequestMapping("/api/trip-cards")
 @RequiredArgsConstructor
 public class TripCardController {
 
     private final TripCardService tripCardService;
+    private final TripCardEntryService tripCardEntryService;
 
 
     @GetMapping
@@ -35,5 +39,18 @@ public class TripCardController {
         return tripCardService.deleteTripCard(tripCardIds);
     }
 
+
+    @PostMapping("/{cardId}/entries")
+    public TripCardEntryResponse addEntry(@PathVariable Long cardId,
+             @RequestPart("imageFile") MultipartFile imageFile,
+             @RequestPart(value = "comment", required = false) String comment
+    ) {
+        return tripCardEntryService.addEntry(cardId, imageFile, comment);
+    }
+
+    @DeleteMapping("/{cardId}/entries/{entryId}")
+    public void deleteEntry(@PathVariable Long cardId, @PathVariable Long entryId) {
+        tripCardEntryService.deleteEntry(cardId, entryId);
+    }
 
 }
